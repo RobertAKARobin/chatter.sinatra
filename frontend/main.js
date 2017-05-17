512f8d460ff6763b71b0e7ccb940a6ca611ee7af
RobertAKARobin <robertgfthomas@gmail.com>
2017-05-17 00:11:12 -0500

    Added listener for when page loads

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,4 @@
-console.log('This is in main.js!');
+document.addEventListener('DOMContentLoaded', function(){
+	console.log('This is in main.js!');
+	document.getElementById('title').style.color = 'red';
+});
